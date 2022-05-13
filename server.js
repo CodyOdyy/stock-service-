daa20477fd8fce60a6a3470f20863c7a63115198
Oshadi Dilanka <oshidilanka99@outlook.com>
2022-05-13 06:33:57 +0530

    com25

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const con = mysql.createConnection({
     database: "users",
     username: "root",
     password: "Dilanka123",
-    socketPath: "/cloudsql/My First Project :us-central1:root",
+    socketPath: "/cloudsql/My First Project:us-central1:root",
     dialect: "mysql",
     dialectOptions: {
-          "socketPath": "/cloudsql/My First Project :us-central1:root"
+          "socketPath": "/cloudsql/My First Project:us-central1:root"
     } });
 
 const init = async ()=> {
